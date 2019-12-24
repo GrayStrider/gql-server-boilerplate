0471e510897e7f2bd9eeb7003ed19622389dea0e
Gray Strider <zhoga.ivan@gmail.com>
2019-12-25 06:56:41 +1100

    search by task parameter, pattern matching

--- src/entity/KBF/Task.ts
+++ src/entity/KBF/Task.ts
@@ -1,19 +1,30 @@
 import {Field, ID, ObjectType} from 'type-graphql'
 import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
 
+/*
+ export interface ITask {
+ id: string
+ title: string
+ description: string
+ }
+ */
+
 @ObjectType()
 @Entity()
-export class Task extends BaseEntity {
+export class Task extends BaseEntity/* implements ITask*/ {
 	@Field(() => ID)
-	@PrimaryGeneratedColumn("uuid")
-	id: number
+	@PrimaryGeneratedColumn('uuid')
+	id: string
 	
 	@Field()
-	@Column()
+	@Column({length: 100})
 	title: string
 	
 	@Field()
-	@Column({nullable: true})
+	@Column({length: 1000, nullable: true})
 	description: string
 	
+	@Field()
+	@Column({type: 'bool', default: false})
+	completed: boolean
 }
--- src/modules/KBF/index.ts
+++ src/modules/KBF/index.ts
@@ -1,19 +1,52 @@
-import {Arg, Args, ArgsType, Field, InputType, Mutation, Query, Resolver} from 'type-graphql'
+import {Max} from 'class-validator'
+import {Args, ArgsType, Field, Mutation, Query, Resolver} from 'type-graphql'
+import {Like} from 'typeorm'
 import {Task} from '../../entity/KBF/Task'
+
 @ArgsType()
 class NewTaskInput implements Partial<Task> {
 	@Field()
 	title: string
+	
+	// on field nullable only applicable in the context of inputs
+	@Field({nullable: true})
+	description: string
 }
+
+@ArgsType()
+class SearchTaskInput implements Partial<Task> {
+	@Field({nullable: true})
+	@Max(100)
+	title: string
+	
+	@Field({nullable: true})
+	description: string
+	
+	@Field({nullable: true})
+	id: string
+	
+	@Field({nullable: true})
+	completed: boolean
+}
+
+const Like_: any = (a: { [key: string]: any }, b: string) =>
+	({[b]: Like(`%${a[b]}%`) as unknown as string})
+
 
 @Resolver()
 export class TaskResolver {
 	@Query(returns => Array(Task))
-	tasks() { return Task.find()}
+	async tasks(@Args() params: SearchTaskInput) {
+		return await Task.find(
+			params.title ? Like_(params, 'title') :
+				params.description ? Like_(params, 'description') :
+					params
+		)
+	}
 	
 	@Mutation(returns => Task)
-	async taskCreate(@Args() {title}: NewTaskInput) {
-		return await Task.create({title}).save()
+	async taskCreate(@Args() data: NewTaskInput) {
+		return await Task.create(data).save()
 	}
 	
 	@Mutation(returns => Array(Task))
