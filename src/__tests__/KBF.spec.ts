687c1f172a3113f13106b1910bb98813832f6943
Gray Strider <zhoga.ivan@gmail.com>
2019-12-26 04:40:51 +1100

    fix local DB

--- src/__tests__/KBF.spec.ts
+++ src/__tests__/KBF.spec.ts
@@ -1,31 +1,29 @@
-process.env.NODE_ENV = 'development' // TODO fix local postgres and switch to test
+// process.env.NODE_ENV = 'development'
 
 import {ASTNode, print} from 'graphql'
 import request from 'graphql-request'
 import gql from 'graphql-tag'
-import * as http from 'http'
-import {Connection, createConnection} from 'typeorm'
+import {Connection, createConnection, getConnection} from 'typeorm'
 import {HOST, PORT} from '../../config/consts'
 import {ORMConfig} from '../../config/typeorm'
-import {Task} from '../entity/KBF/Task'
 import {main} from '../server'
+import {warn} from '../utils/log'
 
 let conn: Connection
-let server: http.Server
 const url = `http://${HOST}:${PORT}/graphql`
-
-
 const postQuery = async <T>(query: ASTNode, mainField?: string) => {
 	const res: {[key: string]: any} = await request(url, print(query))
 	return mainField ? res?.[mainField]: res }
 
 beforeAll(async () => {
+	warn("testing env:" + process.env.NODE_ENV)
+	warn(ORMConfig)
 	jest.setTimeout(30000)
-	// server = await main()
-	
-	conn = await createConnection(ORMConfig) // get connection, if starting server from here
-	
-	await conn.synchronize(true)
+	await main()
+	conn = await getConnection() ?? await createConnection(ORMConfig)
+
+	await conn.dropDatabase()
+	await conn.synchronize()
 })
 
 afterAll(async () => {
