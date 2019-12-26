639c8494bd60c764369699c2449c94448ec8f83c
Gray Strider <zhoga.ivan@gmail.com>
2019-12-26 19:55:23 +1100

    typeorm examples

--- src/__typeorm reference/Entity.ts
+++ src/__typeorm reference/Entity.ts
@@ -1,5 +1,19 @@
-import {Field, ObjectType} from 'type-graphql'
+import {Field, Int, InterfaceType, ObjectType} from 'type-graphql'
 import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from 'typeorm'
+
+
+//================================================================================
+// Helpers
+//================================================================================
+
+@ObjectType()
+class SimpleJSONObjectTypeInterface {
+	@Field()
+	name: string
+	
+	@Field()
+	age: number
+}
 
 //================================================================================
 // Entity
@@ -23,5 +37,15 @@ export class ExampleEntity extends BaseEntity {
 	@Column()
 	isActive: boolean
 	
+	// don't use simple-array
+	// note the default syntax
+	// or use enums
+	@Field(returns => [Int])
+	@Column('int', {array: true, default: '{1, 2, 3}'})
+	array: number[]
+	
+	@Field(returns => SimpleJSONObjectTypeInterface)
+	@Column('simple-json', {default: {name: "Ivan", age: 24}})
+	json: { name: string, age: number }
 }
 
