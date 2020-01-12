b53077060d53886f1ba89026934e89d58f8d2851
Gray Strider <zhoga.ivan@gmail.com>
2020-01-12 14:48:22 +1100

    Remove | Express

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,17 @@
-import chalk from 'chalk'
-import {main} from './server'
-import * as Sentry from '@sentry/node'
 import {log} from './utils/libsExport'
-import {KoaServer} from '@/server.koa'
+import {KoaServer} from '@/server'
 
 
 //================================================================================
 // Main server module is exposed for use in testing
 //================================================================================
 
-process.on('uncaughtException', (error)  => {
+process.on('uncaughtException', (error) => {
 	
 	log.error(`uncaught Exception: ${error.message}`)
 	log.error(error)
 	
-	process.exit(1); // exit application
+	process.exit(1) // exit application
 	
 })
 
@@ -22,13 +19,6 @@ process.on('uncaughtException', (error)  => {
 process.on('unhandledRejection', (error, promise) => {
 	log.error(`unhandledRejection: ${error.message}`)
 	log.error(error)
-});
+})
+
 KoaServer().catch(log.error)
-
-// only at initialization!
-// main().catch((err) => {
-// 	Sentry.captureException(err)
-// 	console.error(chalk.red.bold(err)) })
-
-
-
