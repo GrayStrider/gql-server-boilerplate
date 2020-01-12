484e36af1cf72c10f8452f64ee2844011c7651bb
Gray Strider <zhoga.ivan@gmail.com>
2020-01-12 20:46:18 +1100

    Routing | better routing organization

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import Router from 'koa-router'
+import {DefaultState, Context} from 'koa'
+
+const router = new Router<DefaultState, Context>()
+
+router.get('/', (ctx, next) => {
+	ctx.body = 'Hello World!'
+})
+
+export {router}
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import 'reflect-metadata'
-import Koa, {DefaultState, Context} from 'koa'
+import Koa from 'koa'
 import helmet from 'koa-helmet'
 import session from 'koa-session'
 import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
-import Router from 'koa-router'
 import {log} from '@/utils/libsExport'
 import {useContainer, createConnection} from 'typeorm'
 import {Container} from 'typedi'
@@ -14,10 +13,10 @@ import {redis} from '@/DB/redis'
 import {PORT, HOST} from 'config/_consts'
 import {usersServer} from '@/models/UsersPlayground'
 import {plainSchemaServer} from '@/models/PlainSchema'
+import {router} from '@/routes'
 
 export async function KoaServer() {
 	const app = new Koa()
-	const router = new Router<DefaultState, Context>()
 	
 	useContainer(Container)
 	const conn = await createConnection(ORMConfig)
@@ -25,28 +24,22 @@ export async function KoaServer() {
 		log.warn('resetting the DB')
 		await conn.synchronize(true)
 	}
+
+	app
+		.use(session({
+			store: RedisStore({
+				client: redis,
+			}),
+			key: 'redisCookie',
+		}, app))
+		.use(helmet({}))
+		.use(cors({}))
+		.use(bodyParser({}))
+		.use(await usersServer())
+		.use(plainSchemaServer())
+		.use(router.routes())
+		.use(router.allowedMethods({}))
 	
-	app.use(session({
-		store: RedisStore({
-			client: redis,
-		}),
-		key: 'redisCookie',
-	}, app))
-	
-	app.use(helmet({}))
-	app.use(cors({}))
-	app.use(bodyParser({}))
-	
-	router.get('/', (ctx, next) => {
-		ctx.body = 'Hello World!'
-	})
-	
-	app.use(router.routes())
-	app.use(router.allowedMethods({}))
-	
-	
-	app.use(await usersServer())
-	app.use(plainSchemaServer())
 	
 	return app
 		.listen(PORT, () => {
