65dd264383719fc138af5a530ef78e1d5037a4f5
Gray Strider <zhoga.ivan@gmail.com>
2020-01-16 21:20:56 +1100

    Del | Original

--- src/models/Original/modules/user/changePassword/ChangePasswordInput.ts
+++
@@ -1,8 +0,0 @@
-import {Field, InputType} from 'type-graphql'
-import {PasswordMixin} from '../../shared/PasswordInput'
-
-@InputType()
-export class ChangePasswordInput extends PasswordMixin(class {}) {
-	@Field()
-	token: string
-}
