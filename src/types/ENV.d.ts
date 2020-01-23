510e945cf4200c464a4b03e8fb0ca5e28f06d96f
Gray Strider <zhoga.ivan@gmail.com>
2020-01-23 22:11:08 +1100

    Lint | test

--- src/types/ENV.d.ts
+++ src/types/ENV.d.ts
@@ -2,8 +2,8 @@ declare namespace NodeJS {
 	export interface ProcessEnv {
 
 		NODE_ENV: 'development' | 'production' | 'test'
-		POSTGRES_URL?: string;
-		POSTGRES_PORT?: number;
+		POSTGRES_URL?: string
+		POSTGRES_PORT?: number
 	}
 
 }
