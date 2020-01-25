7839062b75e4c04ebb1c37f0ccdea9062fc760df
Gray Strider <zhoga.ivan@gmail.com>
2020-01-25 20:46:24 +1100

    Refactor | move exports closer

--- config/_consts.ts
+++ config/_consts.ts
@@ -5,17 +5,30 @@ process.env.NODE_CONFIG_DIR = __dirname
 process.env.ALLOW_CONFIG_MUTATIONS = 'true'
 
 if (isNil(process.env.NODE_ENV)) throw new Error('process.env is undefined, aborting')
-export const {NODE_ENV} = process.env
+const {NODE_ENV} = process.env
 
 
-export const PORT: string = get('PORT')
-export const HOST: string = get('HOST')
-export const POSTGRES_URL: string = process.env.POSTGRES_URL ?? get('postgres.url')
-export const POSTRGRES_PORT = Number(process.env.POSTGRES_PORT ?? get('postgres.port'))
-export const POSTRGRES_PASSWORD: string = process.env.POSTRGRES_PASSWORD ?? get('postgres.password')
-export const POSTRGRES_USERNAME: string = process.env.POSTRGRES_USERNAME ?? get('postgres.username')
-export const POSTRGRES_DATABASE: string = process.env.POSTRGRES_DATABASE ?? get('postgres.database')
-export const GQL_URL = `http://${HOST}:${PORT}/${process.env.endpoint ?? 'graphql'}`
-export const SERVER_URL = `http://${HOST}:${PORT}`
-export const APOLLO_ENGINE_API_KEY: string = process.env.ENGINE_API_KEY ?? 'not provided'
+const PORT: string = get('PORT')
+const HOST: string = get('HOST')
+const POSTGRES_URL: string = process.env.POSTGRES_URL ?? get('postgres.url')
+const POSTRGRES_PORT = Number(process.env.POSTGRES_PORT ?? get('postgres.port'))
+const POSTRGRES_PASSWORD: string = process.env.POSTRGRES_PASSWORD ?? get('postgres.password')
+const POSTRGRES_USERNAME: string = process.env.POSTRGRES_USERNAME ?? get('postgres.username')
+const POSTRGRES_DATABASE: string = process.env.POSTRGRES_DATABASE ?? get('postgres.database')
+const GQL_URL = `http://${HOST}:${PORT}/${process.env.endpoint ?? 'graphql'}`
+const SERVER_URL = `http://${HOST}:${PORT}`
+const APOLLO_ENGINE_API_KEY: string = process.env.ENGINE_API_KEY ?? 'not provided'
 
+export {
+	NODE_ENV,
+	PORT,
+	HOST,
+	POSTGRES_URL,
+	POSTRGRES_PORT,
+	POSTRGRES_PASSWORD,
+	POSTRGRES_USERNAME,
+	POSTRGRES_DATABASE,
+	GQL_URL,
+	SERVER_URL,
+	APOLLO_ENGINE_API_KEY
+}
