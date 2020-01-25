275e57e6ef7507ba1d8caf1af00e36a2a2ff47c1
Gray Strider <zhoga.ivan@gmail.com>
2020-01-26 01:47:49 +1100

    Refactor | Users Resolvers

--- src/models/UsersPlayground/index.ts
+++ src/models/UsersPlayground/index.ts
@@ -4,15 +4,24 @@ import generateMockUsers from '@/models/UsersPlayground/lib/generateMockUsers'
 import howCommonIsName from '@/models/UsersPlayground/lib/HowCommonName'
 import UserNew from '@/models/UsersPlayground/entity/User'
 import Countries from '@/models/UsersPlayground/lib/CountriesList'
-import UserResolver from '@/models/UsersPlayground/user.resolver'
 import createSchema from '@/graphql/type-graphql/createSchema'
 import genericApolloServer from '@/graphql/apollo/genericServer'
+import {UsersResolver, RegisterResolver, ModifyResolver, LoginResolver, GenerateMocksResolver, CreateResolver} from '@/models/UsersPlayground/resolvers'
 
 export default async function makeUsersServer () {
 	
 	const name = 'users'
 	const path = `/${name}`
-	const schema = await createSchema([UserResolver], name)
+	const schema = await createSchema([
+		
+		CreateResolver,
+		GenerateMocksResolver,
+		LoginResolver,
+		ModifyResolver,
+		RegisterResolver,
+		UsersResolver,
+	
+	], name)
 	sig.info(SERVER_URL + path)
 	return genericApolloServer(schema)
 		.getMiddleware({path})
@@ -22,9 +31,7 @@ export default async function makeUsersServer () {
 export {
 	UserNew,
 	generateMockUsers,
-	UserResolver,
 	howCommonIsName,
 	Countries
-	
 }
 
--- src/models/UsersPlayground/lib/index.ts
+++ src/models/UsersPlayground/lib/index.ts
@@ -1,7 +1,23 @@
 import {IsEmail} from 'class-validator'
 import {UserNew} from '@/models/UsersPlayground'
+import {Errors} from '@/utils'
 
 const IsValidEmail = IsEmail({}, {message: 'Invalid email format'})
+const birthYearFromAge = (age: number) => new Date().getFullYear() - age
+
+const validatePassword = (password: string): true => {
+
+	if (password.length < 6) throw new Errors.Validation('Password must contain...')
+	
+	return true
+	
+}
+
 type UserCreateType = Partial<Omit<UserNew, 'age'>> & { age: number }
 
-export {IsValidEmail, UserCreateType}
+export {
+	IsValidEmail,
+	UserCreateType,
+	birthYearFromAge,
+	validatePassword
+}
