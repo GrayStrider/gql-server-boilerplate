2019a9fd2721e18bcc095f0fb4904fd4dda1933e
Gray Strider <zhoga.ivan@gmail.com>
2020-01-28 01:39:43 +1100

    :fire: old postQuery

--- src/graphql/utils/postQuery.ts
+++ src/graphql/utils/postQuery.ts
@@ -2,29 +2,10 @@ import {ASTNode, print} from 'graphql'
 import {request} from 'graphql-request'
 import {Variables} from 'graphql-request/dist/src/types'
 import {AnyObject} from 'tsdef'
-import {isNil} from 'ramda'
 import {GQL_URL} from '@config'
 import {Mutation, Query} from '@/graphql/generated/typings'
 import {flattenObject} from '@/utils'
 
-
-/**
- * Dispatch GQL query
- * @param url server url, defaults to GQL_URL env. variable
- * @param query graphql-tag gql\`{...}\` query
- * @param mainField select one field from respose object:
- * query "tasks" will respond with object `{tasks: [...]}`, and we would have to do `.then(value => value["tasks"])`.
- *   Specifying `"tasks"` in mainField parameter will return the array right away
- */
-
-async function postQuery<T = AnyObject[]> (
-	query: ASTNode, mainField?: string, url: string = GQL_URL
-): Promise<{ [key: string]: T }> {
-	
-	const res: AnyObject = await request(url, print(query))
-	return isNil(mainField) ? res : res?.[mainField]
-	
-}
 
 async function gqlRequest<T> (query: ASTNode, variables?: Variables, url?: string): Promise<T>
 async function gqlRequest<T, K> (query: ASTNode, variables?: Variables, url?: string): Promise<[T, K]>
