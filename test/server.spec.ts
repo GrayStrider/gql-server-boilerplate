c0f72ae7b60c5824c1dead65c9f5e943506b28f1
Gray Strider <zhoga.ivan@gmail.com>
2020-01-29 02:14:27 +1100

    :bug: supertest jest open handle fix

--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,23 +1,31 @@
 import supertest, {SuperTest, Test} from 'supertest'
 import main from 'src/server'
+import {sleep} from 'src/utils'
 
 describe('server', () => {
-
+	
 	let request: SuperTest<Test>
 	
 	beforeAll(async () => {
-
+		
 		request = supertest((await main()).callback())
+		
+	})
+	
+	afterAll(async () => {
+
+		// Avoid jest open handle error
+		await sleep(500)
 	
 	})
-
+	
 	it('should start the server', async () => {
-
+		
 		expect.assertions(2)
 		const res = await request.get('/')
 		expect(res.status).toBe(200)
 		expect(res.text).toBe('Hello World!')
-
+		
 	})
 
 })
