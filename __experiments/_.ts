f4db714f33452445f513303cf568c74397ece5d8
Gray Strider <zhoga.ivan@gmail.com>
2020-02-09 14:05:58 +1100

    repos

--- __experiments/_.ts
+++ __experiments/_.ts
@@ -1,9 +1,27 @@
 import {truncate} from 'lodash'
+import {axios} from '@/utils'
+import {map, prop, then, join, length, tap, pipe, pick, reduce, pickBy, append} from 'ramda'
+import {AnyObject} from 'tsdef'
 
-function main () {
+async function main () {
 	
-	console.log(truncate(undefined, {length: 5}).length)
+	const {data} = await axios.get<AnyObject[]>('https://api.github.com/users/GrayStrider/repos')
+	
+	const repos = data.reduce(
+		(acc, curr) => {
+			if (curr.archived) acc.archived.push(curr.name)
+			else acc.repos.push(curr.name)
+			
+			return acc
+		},
+		{
+			archived: [],
+			repos: []
+		}
+	)
+	
+	return repos
 	
 }
 
-main()
+main().then(console.log)
