9781d7d20a950f5724a07be14f68598fa932e5e5
Gray Strider <zhoga.ivan@gmail.com>
2020-02-12 23:52:17 +1100

    random weathers

--- __experiments/DB/index.spec.ts
+++ __experiments/DB/index.spec.ts
@@ -2,6 +2,8 @@ import main from '@/server'
 import supertest from 'supertest'
 import {SuperTest, Test, sleep} from '@/utils'
 import {Task, Weather, Cities} from '@/models'
+import {times, keys, min} from 'ramda'
+import * as faker from 'faker'
 
 let request: SuperTest<Test>
 beforeAll(async () => {
@@ -32,5 +34,24 @@ describe('DB calls', () => {
 		})
 		await SFW.save()
 		expect(await Weather.findOne()).toMatchObject(SFW)
+	})
+	
+	it('should generate fake data', async () => {
+		expect.assertions(1)
+		const weathers = times(() => Weather.create({
+				city: faker.random.arrayElement(keys(Cities)) as Cities,
+				...function () {
+					const temps = times(() =>
+						faker.random.number(100), 2)
+					return {
+						temp_lo: Math.min(...temps),
+						temp_hi: Math.max(...temps)
+					}
+				}(),
+				prcp: faker.random.number({max: 1, precision: 0.1}),
+				date: faker.date.past(10)
+			}
+		), 100)
+		console.log(weathers)
 	})
 })
