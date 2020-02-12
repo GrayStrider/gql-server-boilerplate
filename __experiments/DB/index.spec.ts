5cb712a16bf67d3f0fcc6e3bd822353737184fce
Gray Strider <zhoga.ivan@gmail.com>
2020-02-12 22:42:36 +1100

    :white_check_mark: DB connection

--- __experiments/DB/index.spec.ts
+++ __experiments/DB/index.spec.ts
@@ -1,18 +1,16 @@
+import main from '@/server'
 import supertest from 'supertest'
-import main from 'src/server'
 import {SuperTest, Test, sleep} from '@/utils'
+import {Task} from '@/models'
 
 let request: SuperTest<Test>
-
 beforeAll(async () => {
 	jest.setTimeout(99999)
 	request = supertest((await main()).callback())
 })
-
 afterAll(async () => {
 	await sleep(500)
 })
-
 describe('setup', () => {
 	it('should initialize connection', async () => {
 		expect.assertions(1)
@@ -20,4 +18,12 @@ describe('setup', () => {
 		expect(status).toBe(200)
 	})
 	
+})
+
+describe('DB calls', () => {
+	it('should create entity', async () => {
+		expect.assertions(1)
+	  const tasks = await Task.find()
+		expect(tasks).toStrictEqual([])
+	})
 })
