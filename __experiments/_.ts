df8730bcd79e93ab636ed1a6b3628cef1241c6bf
Gray Strider <zhoga.ivan@gmail.com>
2020-02-14 05:13:54 +1100

    chore() push test

--- __experiments/_.ts
+++ __experiments/_.ts
@@ -4,12 +4,13 @@ import {reduceBy} from 'ramda'
 async function main () {
 	interface Repo {
 		archived: boolean
+		
 		name: string
 	}
 	
 	const {data} = await axios.get<Repo[]>
 	('https://api.github.com/users/GrayStrider/repos')
-
+	
 	return reduceBy(
 		(acc, {name}) => acc.concat(name),
 		[] as string[],
