6ef9fb327751bfc4713a7a7e76c9dd94ba5be94e
Gray Strider <zhoga.ivan@gmail.com>
2020-02-15 14:09:43 +1100

    fix(jest): supertest expect fix

---
+++ test/e2e/app.spec.ts
@@ -0,0 +1,27 @@
+import request from 'supertest'
+import { Test } from '@nestjs/testing'
+import { AppModule } from 'src/app.module'
+import { INestApplication } from '@nestjs/common'
+import { sleep } from 'src/utils'
+
+describe('AppController', () => {
+	let app: INestApplication
+	
+	beforeAll(async () => {
+		const moduleFixture = await Test.createTestingModule({
+			imports: [AppModule]
+		}).compile()
+		
+		app = moduleFixture.createNestApplication()
+		await app.init()
+	})
+	afterAll(async () => await sleep(500))
+	
+	it('/ (GET)', async () => {
+		expect.assertions(2)
+    const server = app.getHttpServer()
+    const {status, text} = await request(server).get('/')
+    expect(status).toBe(200)
+    expect(text).toBe('Hello World!')
+	})
+})
