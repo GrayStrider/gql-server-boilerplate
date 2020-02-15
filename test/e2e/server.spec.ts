fa41d455a8051207855d602b6e0881dd46cf9553
Gray Strider <zhoga.ivan@gmail.com>
2020-02-15 14:49:12 +1100

    feat(jest): e2e config

--- test/e2e/server.spec.ts
+++ test/e2e/server.spec.ts
@@ -1,31 +1,22 @@
-import {ASTNode} from 'graphql'
+import { ASTNode } from 'graphql'
 import main from 'src/server'
-import {sleep, supertest, Test, SuperTest} from 'src/utils'
+import { sleep, supertest, Test, SuperTest } from 'src/utils'
 
 describe('server', () => {
 	
-	let request: SuperTest<Test>
-	let post: <T>(query: ASTNode) => Promise<T>
+	let request: ReturnType<typeof supertest>['request']
+	let post: ReturnType<typeof supertest>['post']
 	
 	beforeAll(async () => {
-		
 		({request, post} = supertest(await main(), '/example'))
-		
 	})
 	
-	afterAll(async () => {
-		
-		// Avoid jest open handle error
-		await sleep(500)
-		
-	})
+	afterAll(async () => await sleep(500))
 	
 	it('should start the server and redirect', async () => {
-		
 		expect.assertions(1)
 		const {status} = await request.get('/foobar')
 		expect(status).toBe(302)
-		
 	})
-
+	
 })
